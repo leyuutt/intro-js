ecbc3b341d5a69325486ea7365dcdfb6f4b6910d
leyuutt <edlawittesfaye73@gmail.com>
2024-08-01 13:57:45 +0300

    reminder

--- lower.js
+++ lower.js
@@ -1,6 +1,10 @@
-const u=8
-const y =9 
-let reslt 
-console.log(u+y)
-console.log(u/y)
-console.log(u*y)
+// const u=8
+// const y =9 
+// let reslt 
+// console.log(u+y)
+// console.log(u/y)
+// console.log(u*y)
+for(let w=0 ; w<7 ; w++){
+    // console.log(`thi is ${w} th iteration`,w )
+    if(w%2!==0){console.log(w)}
+}
