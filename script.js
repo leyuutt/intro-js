836774bde5efd10710a9fdc604fd6aff7dc3b95a
leyuutt <edlawittesfaye73@gmail.com>
2024-08-06 13:02:03 +0300

    working on loops

---
+++ script.js
@@ -0,0 +1,19 @@
+// for loop
+
+//  for (i=1 ; i <=5; i++){
+//      console.log(i)
+//      for(j=1;j <=3; j++){
+//         console.log("Inner lopp"+ j)
+//      }
+//  }
+// let coding = ["JavaScript", "Python", "CPP"]
+// for (let i=0; i<coding.length; i++){
+//     console.log(coding[i])
+// }
+
+ for (let i = 1; i <= 5; i++){
+    if(i == 3){
+        continue;
+    }
+    console.log(i)
+}    
